2219c29053630f7b94ff0ac283790566220097f7
joshuakobuskie <joshkobuskie@gmail.com>
2024-11-23 13:16:17 -0500

    style: end line

--- src/split_data.py
+++ src/split_data.py
@@ -34,7 +34,3 @@ def train_test_split(dataframe, target, test_size=0.1, rand_state=None):
     train_dataframe = dataframe.loc[train_indexes].reset_index(drop=True)
     test_dataframe = dataframe.loc[test_indexes].reset_index(drop=True)
     return train_dataframe, test_dataframe
-
-# Testing
-# dataframe = pandas.read_csv("../data/Health_Sleep_Statistics.csv")
-# print(train_test_split(dataframe, target="Sleep Quality"))

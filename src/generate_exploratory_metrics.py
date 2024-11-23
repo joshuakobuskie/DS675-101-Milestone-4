b10dd653a1a8c5f041afe981ff0d3c3b5815cc21
Spencer Clarke <sdc2@njit.edu>
2024-11-23 06:24:52 +0000

    Increased resolution of correlation matrix

--- src/generate_exploratory_metrics.py
+++ src/generate_exploratory_metrics.py
@@ -5,16 +5,16 @@ def generate_exploratory_metrics(dataframe):
 	correlation_matrix = dataframe.corr()
 	feature_names = dataframe.columns.to_numpy()
 	
-	fig, ax = pyplot.subplots(figsize=(6, 5))
+	fig, ax = pyplot.subplots(figsize=(10, 10))
 	pyplot.title('Correlation matrix')
-	pyplot.xticks(rotation=90, ticks=range(0, 11), labels = feature_names)
-	pyplot.yticks(ticks=range(0, 11), labels = feature_names)
+	pyplot.xticks(rotation=90, ticks=range(len(feature_names)), labels = feature_names, fontsize=15)
+	pyplot.yticks(ticks=range(len(feature_names)), labels = feature_names, fontsize=15)
 	pyplot.imshow(correlation_matrix)
 	pyplot.tight_layout()
 	
-	for i in range(11):
-		for j in range(11):
+	for i in range(len(feature_names)):
+		for j in range(len(feature_names)):
 			value = correlation_matrix[feature_names[i]][feature_names[j]]
-			ax.text(j, i, f'{value:.2f}', ha='center', va='center', fontsize=7, color = 'black' if value > 0 else 'white')
+			ax.text(j, i, f'{value:.2f}', ha='center', va='center', fontsize=15, color = 'black' if value > 0 else 'white')
 
 	pyplot.savefig('../assets/correlation_matrix.jpg')

2213002aee09e8c0de8dda24835dd92c58e2d9e3
joshuakobuskie <joshkobuskie@gmail.com>
2024-11-23 13:54:07 -0500

    feat: add confusion matrix and truth table

--- src/performance_metrics.py
+++ src/performance_metrics.py
@@ -16,7 +16,30 @@ def get_confusion_matrix(y_true, y_pred):
     
     return confusion_matrix
 
+def get_true_false_positives_negatives(confusion_matrix):
+    classes = len(confusion_matrix)
+    
+    truth_table = {class_label: {"true_positives": 0, "false_positives": 0, "true_negatives": 0, "false_negatives": 0} for class_label in range(classes)}
+
+    for class_index in range(classes):
+        true_positives = confusion_matrix[class_index][class_index]
+        false_positives = sum(confusion_matrix[row][class_index] for row in range(classes)) - true_positives
+        false_negatives = sum(confusion_matrix[class_index]) - true_positives
+        true_negatives = sum(sum(confusion_matrix[row][col] for col in range(classes)) for row in range(classes)) - (true_positives + false_positives + false_negatives)
+
+        truth_table[class_index]["true_positives"] = true_positives
+        truth_table[class_index]["false_positives"] = false_positives
+        truth_table[class_index]["true_negatives"] = true_negatives
+        truth_table[class_index]["false_negatives"] = false_negatives
+
+    return truth_table
+    
+
+
 def get_performance_metrics(y_true, y_pred, y_prob):
     
     confusion_matrix = get_confusion_matrix(y_true, y_pred)
-    
+
+    truth_table = get_true_false_positives_negatives(confusion_matrix)
+
+    print(truth_table)
